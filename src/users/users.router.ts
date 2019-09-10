b2d82eae73733a1293182131d28a6b3784c832c1
Artho Valentim Barbosa Petry <artho@ampliato.com.br>
2019-09-10 07:39:21 -0300

    adicionando metodo put

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -1,6 +1,7 @@
 import {Router} from '../common/router';
 import  *  as restify from 'restify';
 import {User} from './users.model';
+import { response } from 'spdy';
 
 class UsersRouter extends Router {
     applyRoutes(application: restify.Server) {
@@ -30,6 +31,23 @@ class UsersRouter extends Router {
                   res.json(user);
                   return next();
               });
+        });
+
+        application.put('/users/:id', (req, res, next) => {
+            const options = {overwrite: true}            
+            User.update({_id: req.params.id}, req.body, options)
+               .exec()
+               .then( result => {
+                   if (result.n) {
+                      return User.findById(req.params.id) 
+                   } else {
+                       res.send(404)
+                   }
+               })
+               .then(user => {
+                   res.json(user);
+                   return next();
+               }) 
         });
     }
 }
